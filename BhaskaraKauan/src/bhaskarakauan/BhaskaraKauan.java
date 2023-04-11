01bdb718e5d135b60c05b953a225af89fbe2c279
TeslaKauan <129294498+TeslaKauan@users.noreply.github.com>
2023-04-11 08:34:07 -0300

    Add files via upload

---
+++ BhaskaraKauan/src/bhaskarakauan/BhaskaraKauan.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bhaskarakauan;
+
+/**
+ *
+ * @author escola
+ */
+public class BhaskaraKauan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       Kauan b1 = new Kauan();
+       b1.setA(4);
+       b1.setB(3);
+       b1.setC(-3);
+       b1.ValorDelta();
+       b1.Valorx1();
+       b1.Valorx2();
+       b1.Status();
+    
+
+    }
+    
+}
